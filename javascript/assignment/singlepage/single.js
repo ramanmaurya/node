edf70a8cccb616359bbb3a66cf8185473bea04c1
ramanmaurya <41865346+ramanmaurya@users.noreply.github.com>
2018-09-15 13:00:42 +0530

    Add files via upload

---
+++ javascript/assignment/singlepage/single.js
@@ -0,0 +1,21 @@
+
+
+var ref = document.getElementById("main");
+
+function home(){
+    ref.innerHTML=" how guys how are i don my assignment";
+
+
+}
+
+function about() {
+    ref.innerHTML = " lets start the journy";
+
+}
+
+
+function contact() {
+    ref.innerHTML = " yooo!!!!!!!!!!!!!";
+
+    
+}
